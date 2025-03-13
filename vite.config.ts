77128251336ad9b50c7c43c6c58cd032c789b056
harbouli <mohamed.harbouli.hb@gmail.com>
2025-03-13 22:53:36 +0000

    refactor: restructure project by removing unused files and adding Three.js scene component

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import generouted from "@generouted/react-router/plugin"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+
+import react from "@vitejs/plugin-react"
+import glsl from "vite-plugin-glsl"
 import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted(), svgr()],
-  resolve: { alias: { "@": "/src" } }
+  plugins: [react(), glsl(), generouted(), svgr()],
+  resolve: {
+    alias: {
+      "@": "/src"
+    }
+  }
 })
