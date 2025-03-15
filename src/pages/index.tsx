4657db48582a8d1a265f8862050ed28d4eca3657
Abdou JR <abdou-elboursoumi@outlook.fr>
2025-03-15 22:37:06 +0100

    fix: fixed to dummy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,9 @@ const Index: React.FC = () => {
     <section className="min-h-screen">
       {/* <ThreeScene /> */}
       <div ref={dummy} className="h-[600vh] w-full relative">
-        <div className="fixed top-0 left-0 w-full h-screen bg-purple-300 opacity-25" />
+        <div className="sticky top-0 left-0 w-full h-screen bg-purple-300 opacity-25 z-10" />
       </div>
+      <div className="h-screen w-full bg-red-300">Footer</div>
     </section>
   )
 }
