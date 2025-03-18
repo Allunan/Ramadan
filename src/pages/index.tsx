380b4a57021ac3d087021ed4d94956a671980afa
Gasoil-Gfx <salaheddinehamadi@gmail.com>
2025-03-18 22:18:48 +0000

    adding curve

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,76 @@
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
-import { useEffect, useRef } from "react"
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
+
+const Curve: React.FC<{ progress: number }> = ({ progress }) => {
+  const curve = new THREE.CatmullRomCurve3([
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(5, 0, -5),
+    new THREE.Vector3(0, 0, -10),
+    new THREE.Vector3(-5, 0, -15),
+    new THREE.Vector3(0, 0, -20),
+    new THREE.Vector3(5, 0, -25),
+    new THREE.Vector3(0, 0, -30),
+    new THREE.Vector3(-5, 0, -35),
+    new THREE.Vector3(0, 0, -40),
+  ]);
+  const points = curve.getPoints(50);
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  useFrame((state) => {
+    const camera = state.camera;
+    const point = curve.getPoint(progress);
+    camera.position.set(point.x, point.y, point.z);
+    camera.lookAt(curve.getPoint(progress + 0.01));
+  });
+
+  return (
+    <group>
+      <axesHelper args={[4]} />
+      <line geometry={geometry}>
+        <meshBasicMaterial color={"red"} />
+      </line>
+    </group>
+  );
+};
+
 const Index: React.FC = () => {
-  const dummy = useRef<HTMLDivElement>(null)
+  const dummy = useRef<HTMLDivElement>(null);
+  const [scrollProgress, setScrollProgress] = useState(0);
+
   useEffect(() => {
-    window.scrollTo(0, 0) // Reset scroll on mount if needed
-    gsap.to(dummy.current, {
+    window.scrollTo(0, 0);
+    
+    gsap.to({}, {
       scrollTrigger: {
         trigger: dummy.current,
         start: "top top",
         end: "bottom bottom",
         scrub: true,
         markers: true,
-        onEnter: () => {
-          console.log("Enter")
-        },
         onUpdate: (self) => {
-          console.log(self.progress.toFixed(2))
-        }
-      }
-    })
-  }, [])
+          setScrollProgress(self.progress);
+        },
+      },
+    });
+  }, []);
+
   return (
     <section className="min-h-screen">
-      {/* <ThreeScene /> */}
       <div ref={dummy} className="h-[600vh] w-full relative">
-        <div className="sticky top-0 left-0 w-full h-screen bg-purple-300 opacity-25 z-10" />
+        <div className="sticky top-0 left-0 w-full h-screen">
+          <Canvas>
+            <Curve progress={scrollProgress} />
+          </Canvas>
+        </div>
       </div>
       <div className="h-screen w-full bg-red-300">Footer</div>
     </section>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
